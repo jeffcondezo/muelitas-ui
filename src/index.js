9e99eaaa762724db40a88da8ecaa53051b0630c4
Jeff <jeff_carlos_3040@hotmail.com>
2020-02-25 20:33:18 -0500

    Navigation

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './login/Login.css';
-import Login from './login/Login'
+import Login from './login/Login';
+import Navigation from './components/Navigation';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Navigation />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
